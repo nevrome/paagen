edac66895ef253ea96de63efcf74921f058c69af
Clemens Schmid <clemens@nevrome.de>
2021-03-30 20:34:02 +0200

    some code layout changes

--- src-executables/Main-paagen.hs
+++ src-executables/Main-paagen.hs
@@ -15,7 +15,7 @@ import           System.IO              (hPutStrLn, stderr)
 
 import Data.Maybe (isJust, fromMaybe, catMaybes)
 import Poseidon.Janno
-import Data.List (nub, tails, sortBy, intersect, maximumBy, group, sort)
+import Data.List (nub, tails, sortBy, intersect, maximumBy, group, sort, intercalate)
 import qualified Data.Vector                as V
 import           SequenceFormats.Eigenstrat (EigenstratIndEntry (..),
                                              EigenstratSnpEntry (..), GenoLine,
@@ -89,19 +89,19 @@ runTest (TestOptions test) = do
     -- load janno tables
     let jannos = concatMap posPacJanno allPackages
     -- transform to spatiotemporal positions
-        stInds = jannosToSTInds jannos
+        stInds = jannoToSpaceTimePos jannos
     -- calculate distances
         positionOfInterest = IndsWithPosition "poi" $ SpatialTemporalPosition 1000 (Latitude 47.82) (Longitude 47.82)
         distancesToPoi = distanceOneToAll positionOfInterest stInds
     -- get X closest inds
-        closestInds = getXClosestInds 50 distancesToPoi
+        closestInds = getClosestInds 50 distancesToPoi
+    putStrLn $ "Closest individuals: " ++ intercalate ", " closestInds
     -- determine relevant packages
     relevantPackages <- filterPackagesByInds closestInds allPackages
     indices <- extractIndIndices closestInds relevantPackages
-    print indices
     -- compile genotype data
     runSafeT $ do
-        (eigenstratIndEntries, eigenstratProd) <- getJointGenotypeData True True relevantPackages
+        (eigenstratIndEntries, eigenstratProd) <- getJointGenotypeData False False relevantPackages
         let eigenstratIndEntriesV = V.fromList eigenstratIndEntries
         let newEigenstratIndEntries = [eigenstratIndEntriesV V.! i | i <- indices]
         let [outG, outS, outI] = map ("/home/clemens/test/paagentest" </>) ["huhu.geno", "huhu.snp", "huhu.ind"]
@@ -110,26 +110,71 @@ runTest (TestOptions test) = do
         liftIO $ hClearLine stderr
         liftIO $ hSetCursorColumn stderr 0
         liftIO $ hPutStrLn stderr "SNPs processed: All done"
-    -- 
-    print closestInds
 
-selectIndices :: [Int] -> (EigenstratSnpEntry, GenoLine) -> (EigenstratSnpEntry, GenoLine)
-selectIndices indices (snpEntry, genoLine) = (snpEntry, V.fromList [genoLine V.! i | i <- indices])
+data IndsWithPosition = IndsWithPosition {
+      ind :: String
+    , pos :: SpatialTemporalPosition
+} deriving (Show)
 
-mergeIndividuals :: [Int] -> (EigenstratSnpEntry, GenoLine) -> (EigenstratSnpEntry, GenoLine)
-mergeIndividuals indices (snpEntry, genoLine) = 
-    let relevantGenoEntries = [genoLine V.! i | i <- indices]
-        modeGenoEntry = mostCommon relevantGenoEntries
-    in (snpEntry, V.fromList [modeGenoEntry])
-
-mostCommon :: Ord a => [a] -> a
-mostCommon = head . maximumBy (compare `on` length) . group . sort
+data SpatialTemporalPosition = SpatialTemporalPosition {
+      time :: Int
+    , lat :: Latitude 
+    , lon :: Longitude
+} deriving (Show)
 
 instance Ord GenoEntry where
     compare HomRef Het     = GT
     compare Het HomAlt     = GT
     compare HomAlt Missing = GT
     compare _ _            = EQ
+
+jannoToSpaceTimePos :: [JannoRow] -> [IndsWithPosition]
+jannoToSpaceTimePos jannos = 
+    let filterPos x = isJust (jDateBCADMedian x) && isJust (jLatitude x) && isJust (jLongitude x)
+        transformTo x = IndsWithPosition (jIndividualID x) $
+            SpatialTemporalPosition 
+                (fromMaybe 0 (jDateBCADMedian x)) 
+                (fromMaybe (Latitude 0) (jLatitude x))
+                (fromMaybe (Longitude 0) (jLongitude x))
+    in  map transformTo $ filter filterPos jannos
+
+distanceOneToAll :: IndsWithPosition -> [IndsWithPosition] -> [(String, String, Double)]
+distanceOneToAll poi = 
+    map (distanceOneToOne poi) 
+    where distanceOneToOne i1 i2 = (ind i1, ind i2, spatioTemporalDistance 1 (pos i1) (pos i2))
+
+spatioTemporalDistance :: Double -> SpatialTemporalPosition -> SpatialTemporalPosition -> Double 
+spatioTemporalDistance 
+    scaling
+    (SpatialTemporalPosition t1 (Latitude lat1) (Longitude lon1)) 
+    (SpatialTemporalPosition t2 (Latitude lat2) (Longitude lon2)) =
+    let tDist = fromIntegral (abs (t1 - t2)) * scaling
+        sDist = haversineDist (lat1, lon1) (lat2, lon2)
+    in sqrt $ tDist^2 + sDist^2
+
+haversineDist :: (Double, Double) -> (Double, Double) -> Double 
+haversineDist (lat1, lon1) (lat2, lon2) =
+    let r = 6371000  -- radius of Earth in meters
+        toRadians n = n * pi / 180
+        square x = x * x
+        cosr = cos . toRadians
+        dlat = toRadians (lat1 - lat2) / 2
+        dlon = toRadians (lon1 - lon2) / 2
+        a = square (sin dlat) + cosr lat1 * cosr lat2 * square (sin dlon)
+        c = 2 * atan2 (sqrt a) (sqrt (1 - a))
+    in (r * c) / 1000
+
+getClosestInds :: Int -> [(String, String, Double)] -> [String]
+getClosestInds n dists = map (\(_,x,_) -> x) $ take n $ sortBy (\(_,_,x) (_,_,y) -> compare x y) dists
+
+filterPackagesByInds :: [String] -> [PoseidonPackage] -> IO [PoseidonPackage]
+filterPackagesByInds indNamesStats packages = do
+    fmap catMaybes . forM packages $ \pac -> do
+        inds <- getIndividuals pac
+        let indNamesPac   = [ind   | EigenstratIndEntry ind _ _     <- inds]
+        if  length (intersect indNamesPac indNamesStats) > 0
+        then return (Just pac)
+        else return Nothing
 
 extractIndIndices :: [String] -> [PoseidonPackage] -> IO [Int]
 extractIndIndices indNames relevantPackages = do
@@ -148,68 +193,11 @@ zipGroup list nestedList =
         longerA = map (uncurry replicate) listWithlenghtsNestedList
     in zip3 [0..] (concat longerA) (concat nestedList)
 
-filterPackagesByInds :: [String] -> [PoseidonPackage] -> IO [PoseidonPackage]
-filterPackagesByInds indNamesStats packages = do
-    fmap catMaybes . forM packages $ \pac -> do
-        inds <- getIndividuals pac
-        let indNamesPac   = [ind   | EigenstratIndEntry ind _ _     <- inds]
-        if  length (intersect indNamesPac indNamesStats) > 0
-        then return (Just pac)
-        else return Nothing
+mergeIndividuals :: [Int] -> (EigenstratSnpEntry, GenoLine) -> (EigenstratSnpEntry, GenoLine)
+mergeIndividuals indices (snpEntry, genoLine) = 
+    let relevantGenoEntries = [genoLine V.! i | i <- indices]
+        modeGenoEntry = mostCommon relevantGenoEntries
+    in (snpEntry, V.fromList [modeGenoEntry])
 
-getXClosestInds :: Int -> [(String, String, Double)] -> [String]
-getXClosestInds n dists = map (\(_,x,_) -> x) $ take n $ sortBy (\(_,_,x) (_,_,y) -> compare x y) dists
-
-distanceOneToAll :: IndsWithPosition -> [IndsWithPosition] -> [(String, String, Double)]
-distanceOneToAll poi = map (distanceOneToOne poi)
-
-distanceOneToOne :: IndsWithPosition -> IndsWithPosition -> (String, String, Double)
-distanceOneToOne i1 i2 = (ind i1, ind i2, spatioTemporalDistance 1 (pos i1) (pos i2))
-
--- distanceForPair :: (IndsWithPosition, IndsWithPosition) -> (String, String, Double)
--- distanceForPair (i1, i2) = (ind i1, ind i2, spatioTemporalDistance 1 (pos i1) (pos i2))
-
--- pairs :: [a] -> [(a, a)]
--- pairs l = [(x,y) | (x:ys) <- tails l, y <- ys]
-
-spatioTemporalDistance :: Double -> SpatialTemporalPosition -> SpatialTemporalPosition -> Double 
-spatioTemporalDistance 
-    scaling
-    (SpatialTemporalPosition t1 (Latitude lat1) (Longitude lon1)) 
-    (SpatialTemporalPosition t2 (Latitude lat2) (Longitude lon2)) =
-    let tDist = fromIntegral (abs (t1 - t2)) * scaling
-        sDist = haversineDist (lat1, lon1) (lat2, lon2)
-    in sqrt $ tDist^2 + sDist^2
-
-jannosToSTInds :: [JannoRow] -> [IndsWithPosition]
-jannosToSTInds jannos = 
-    let filterPos x = isJust (jDateBCADMedian x) && isJust (jLatitude x) && isJust (jLongitude x)
-        transformTo x = IndsWithPosition (jIndividualID x) $
-            SpatialTemporalPosition 
-                (fromMaybe 0 (jDateBCADMedian x)) 
-                (fromMaybe (Latitude 0) (jLatitude x))
-                (fromMaybe (Longitude 0) (jLongitude x))
-    in  map transformTo $ filter filterPos jannos
-
-data IndsWithPosition = IndsWithPosition {
-      ind :: String
-    , pos :: SpatialTemporalPosition
-} deriving (Show)
-
-data SpatialTemporalPosition = SpatialTemporalPosition {
-      time :: Int
-    , lat :: Latitude 
-    , lon :: Longitude
-} deriving (Show)
-
-haversineDist :: (Double, Double) -> (Double, Double) -> Double 
-haversineDist (lat1, lon1) (lat2, lon2) =
-    let r = 6371000  -- radius of Earth in meters
-        toRadians n = n * pi / 180
-        square x = x * x
-        cosr = cos . toRadians
-        dlat = toRadians (lat1 - lat2) / 2
-        dlon = toRadians (lon1 - lon2) / 2
-        a = square (sin dlat) + cosr lat1 * cosr lat2 * square (sin dlon)
-        c = 2 * atan2 (sqrt a) (sqrt (1 - a))
-    in (r * c) / 1000
+mostCommon :: Ord a => [a] -> a
+mostCommon = head . maximumBy (compare `on` length) . group . sort
